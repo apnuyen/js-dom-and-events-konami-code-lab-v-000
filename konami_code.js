d44c73ef1cb6cdc7208a573bde2857a41d48e053
An Nuyen <annuyen@gmail.com>
2018-02-03 17:02:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,15 +2,17 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0
 
 function init() {
-  document.body.addEventListener('keydown', function(e) {
+  document.body.addEventListener('keydown', function(e){
     const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
-      index
-      if (index === code.length) {
-        alert("Hurray!")
-        index = 0
-      } else {
+      index++
+
+      if (index === code.length){
+        alert("You did it!")
         index = 0
       }
+    } else {
+      index = 0 
+    }
   })
 }
